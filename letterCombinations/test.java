94cdaf8b5a6c989de01e5f9afe23db0184ad0f09
zhengpengji <123182990+zhengpengji@users.noreply.github.com>
2023-01-29 21:27:35 +0800

    Add files via upload
    给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
    
    给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
    
    
    
     
    
    示例 1：
    
    输入：digits = "23"
    输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
    示例 2：
    
    输入：digits = ""
    输出：[]
    示例 3：
    
    输入：digits = "2"
    输出：["a","b","c"]

---
+++ letterCombinations/test.java
@@ -0,0 +1,9 @@
+package a11;
+
+public class test {
+    public static void main(String[] args) {
+        LetterCombinations letterCombinations =new LetterCombinations();
+        System.out.println(letterCombinations.letterCombinations("52"));
+
+    }
+}
